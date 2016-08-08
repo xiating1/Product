b7004bbd10aa9399bed56bbdd7de789e5427f242
xiating1 <1073144025@qq.com>
2016-08-08 09:56:03 +0800

    修改代码

--- GtiDemo/GtiDemo/ViewController.swift
+++ GtiDemo/GtiDemo/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("在这做点什么")
+        
     }
 
     override func didReceiveMemoryWarning() {
