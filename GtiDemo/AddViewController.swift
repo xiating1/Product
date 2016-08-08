b7ee3612d0b62d0c0bffded54b26bc51f11c5ba0
xiating1 <1073144025@qq.com>
2016-08-08 10:20:46 +0800

    模拟冲突1

--- GtiDemo/AddViewController.swift
+++ GtiDemo/AddViewController.swift
@@ -15,6 +15,17 @@ class AddViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         print("新建的第二文件")
+        //我写的
+        
+        print("有机会一起写代码呀")
+            
+            
+        
+        //别人写的
+        /**我是闹着玩的，你咋不上天呢*/
+        print("产品经理天天该需求,累死大伙儿了，宝宝心里苦~")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
