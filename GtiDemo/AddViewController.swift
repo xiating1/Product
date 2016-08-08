452fd613442c2c5081460588c2d29e84104bb287
xiating1 <1073144025@qq.com>
2016-08-08 10:06:05 +0800

    新建两个ViewController

---
+++ GtiDemo/AddViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddViewController.swift
+//  GtiDemo
+//
+//  Created by 夏婷 on 16/8/8.
+//  Copyright © 2016年 夏婷. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("新建的第二文件")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
